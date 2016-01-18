bef4cdca7090c2b23933eeea7cad23a75088da92
Viah <yjy.viahsta.work@gmail.com>
2016-01-17 18:24:52 -0800

    PHP bug fixed

--- sub-email.php
+++ sub-email.php
@@ -1,16 +1,16 @@
 <?php
-
+	echo 'request made';
 	if (isset($_POST['email'])) {
-		$email = $_POST['email'];
 
+		$email = $_POST['email'];
+		echo $email; 
 		$link = mysqli_connect('localhost','root','',"email_subs");
 		$query = "INSERT INTO email_subs (email) VALUE ('$email')";
 		mysqli_query($link, $query);
 
-		header('Location: index.html');
-
-	} else {
+		header('Location: index-thx.html');
 		echo 'success';
 	}
+
 
 ?> 
